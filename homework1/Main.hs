741cb3111967621bdb54b5f64a7722b085e428e4
NaifAlrashed <naialrashed@alfaisal.edu>
2019-11-20 07:47:52 +0300

    finish exercise 1-4

---
+++ homework1/Main.hs
@@ -0,0 +1,35 @@
+toDigitsRev :: Integer -> [Integer]
+
+toDigitsRev 0 = []
+toDigitsRev x 
+  | isNegative x = []
+  | isSingleInteger x = [x]
+  | otherwise = (x `mod` 10):toDigitsRev (x `div` 10)
+  
+isNegative x = x < 0
+isSingleInteger x = x >= 1 && x <= 9
+
+toDigits :: Integer -> [Integer]
+
+toDigits x = reverse $ toDigitsRev x
+
+doubleEveryOther :: [Integer] -> [Integer]
+
+doubleEveryOther [] = []
+doubleEveryOther (x:y:list) = x:(y * 2):doubleEveryOther list
+doubleEveryOther (x:list) = x:list
+
+sumDigits :: [Integer] -> Integer
+
+sumDigits [] = 0
+sumDigits (singleDigit:digits) = sumValueOfInteger singleDigit + sumDigits digits
+  
+sumValueOfInteger :: Integer -> Integer
+
+sumValueOfInteger number
+  | isSingleInteger number = number
+  | otherwise = (number `mod` 10) + sumValueOfInteger (number `div` 10)
+  
+validate :: Integer -> Bool
+
+validate creditCardNumber = (sumDigits $ doubleEveryOther $ toDigitsRev creditCardNumber) `mod` 10 == 0
