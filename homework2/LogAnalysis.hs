3e0f0351f47f93827f0d3ad9fa2e69611634dbaa
Naif Alrashed <naifdev@gmail.com>
2019-11-22 20:22:45 +0300

    can now parse message type
    but in case of parsing error, there is a not handled exception when trying to parse int out of a string

--- homework2/LogAnalysis.hs
+++ homework2/LogAnalysis.hs
@@ -2,3 +2,17 @@
 module LogAnalysis where
 
 import Log
+
+parseMessage :: String -> LogMessage
+parseMessage [] = Unknown "This is not in the right format"
+
+parseMessageType :: String -> (Maybe MessageType, String)
+
+parseMessageType ['I'] = (Just Info, [])
+parseMessageType ['W'] = (Just Warning, [])
+parseMessageType message = 
+  case (words message) of
+    ("I":list) -> (Just Info, unwords list)
+    ("W":list) -> (Just Warning, unwords list)
+    ("E":potentialErrorNumber:list) -> (Just (Error(read potentialErrorNumber :: Int)), unwords list)
+    _ -> (Nothing, message)
