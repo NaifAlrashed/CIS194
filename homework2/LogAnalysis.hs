9f1b2037d3eb8d6fdfabb349267db878c416fa46
Naif Alrashed <naifdev@gmail.com>
2019-11-25 19:05:38 +0300

    finish parseMessage

--- homework2/LogAnalysis.hs
+++ homework2/LogAnalysis.hs
@@ -4,7 +4,15 @@ module LogAnalysis where
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage [] = Unknown "This is not in the right format"
+
+parseMessage message =
+  case parseMessageType message of
+    (Nothing, _) -> Unknown "This is not in the right format"
+    (Just messageType, restOfMessage) ->
+      case parseTimeStamp restOfMessage of
+        (Nothing, _) -> Unknown "This is not in the right format"
+        (Just timeStamp, whatsLeftOfMessage) -> LogMessage messageType timeStamp whatsLeftOfMessage
+            
 
 parseMessageType :: String -> (Maybe MessageType, String)
 
@@ -14,5 +22,21 @@ parseMessageType message =
   case (words message) of
     ("I":list) -> (Just Info, unwords list)
     ("W":list) -> (Just Warning, unwords list)
-    ("E":potentialErrorNumber:list) -> (Just (Error(read potentialErrorNumber :: Int)), unwords list)
+    ("E":potentialErrorNumber:list) -> (Just (Error(readInt potentialErrorNumber)), unwords list)
     _ -> (Nothing, message)
+    
+parseTimeStamp :: String -> (Maybe TimeStamp, String)
+
+parseTimeStamp message =
+  case words message of
+    (potentialTimeStamp:list) -> (Just $ readInt potentialTimeStamp, unwords list)
+    _ -> (Nothing, message)
+    
+parseRestOfMessage :: String -> (Maybe String, String)
+
+parseRestOfMessage [] = (Nothing, [])
+parseRestOfMessage message = (Just message, [])
+    
+
+readInt :: String -> Int
+readInt = read
